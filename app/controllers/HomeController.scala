364a140f35212a5cfdac44a89ad94c5071dc1c5d
Unknown <yuji.jampan.blue@gmail.com>
2018-03-06 15:07:20 +0900

    Add simple client

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -11,13 +11,12 @@ import play.api.mvc._
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   /**
-   * Create an Action to render an HTML page with a welcome message.
+   * Create an Action to render an HTML page with a welcome message.k
    * The configuration in the `routes` file means that this method
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def ws = Action {
+    Ok(views.html.ws())
   }
-
 }
