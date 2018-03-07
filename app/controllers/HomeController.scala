0d8112780b6d5abf3ec82ade4cf4b3f75f3babb1
nnsnico5214 <yuji.jampan.blue@gmail.com>
2018-03-07 17:28:36 +0900

    Idが`123`の時にdspからレスポンスされた値を返すよう追加

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -9,30 +9,43 @@ import play.api.libs.ws._
 
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
-import scala.util.Success
+import scala.util.{Success, Failure}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
+case class BidResponse(url: String, price: Int)
+
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents, ws: WSClient) extends AbstractController(cc) {
 
   val request: WSRequest = ws.url("http://localhost:8080/main.php")
-  val dspRequest: Int => Future[WSResponse] =
+  val dspRequest: (Int => Future[Option[Seq[JsValue]]]) =
     (id: Int) => request.addHttpHeaders("Accept" -> "application/json")
-                        .withRequestTimeout(50 milliseconds)
+                        .withRequestTimeout(1 seconds)
                         .post(Json.toJson(
                           Map("app_id" -> id)
                         ))
+                        .map {
+                          response => Some(Seq(
+                            (response.json \ "url").get,
+                            (response.json \ "price").get
+                          ))
+                        }
 
   def index = Action { request =>
     val response = for {
       json <- request.body.asJson
       id <- (json \ "app_id").asOpt[Int] if id == 123 if Await.ready(dspRequest(id), Duration.Inf).isCompleted
-    } yield Ok (
-      Json.toJson(Map("app_id" -> id))
-    )
+      result <- Await.result(dspRequest(id), Duration.Inf)
+    } yield {
+      Ok(Json.toJson(Map(
+        "url" -> result.head,
+        "price" -> result(1)
+      )))
+    }
     response.getOrElse(BadRequest("Miss"))
   }
 }
